1f3f2d65344dda719abd59bf04576c2c4b1e9e11
nizam-chenath <nizamstudymajliz@gmail.com>
2021-12-26 22:08:38 +0530

    variables

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const STYLES = ['BTN-PRIMARY', 'BTN-OUTLINE']
 
-const SIZE = ['BTN-SMALL', 'BTN-MEDIUM', 'BTN-MOBILE', 'BTN-WIDE']
+const SIZES = ['BTN-SMALL', 'BTN-MEDIUM', 'BTN-MOBILE', 'BTN-WIDE']
 
 const COLOR = ['PRIMARY', 'RED', 'BLUE', 'GREEN']
 
@@ -14,4 +14,12 @@ export const Button = ({
     buttonStyle,
     buttonSize,
     buttonColor
-});
+}) =>{
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0]
+    const checkButtonColor = STYLES.includes(buttonColor) ? buttonColor : COLOR[0]
+
+    return(
+    <button className={`btn ${checkButtonStyle}${checkButtonSize} ${checkButtonColor}`} onClick={onclick} type={type}>{children}</button>
+    )
+}
